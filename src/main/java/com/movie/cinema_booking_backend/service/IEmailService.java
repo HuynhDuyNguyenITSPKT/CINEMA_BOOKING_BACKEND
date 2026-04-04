20912cba1c2de57f21b3e3a46a51b49f7d42f9f9
HuynhDuyNguyenITSPKT <hdnit3ltvspkt@gmail.com>
2026-04-04 19:47:37 +0700

    them page tra về fe

--- src/main/java/com/movie/cinema_booking_backend/service/IEmailService.java
+++ src/main/java/com/movie/cinema_booking_backend/service/IEmailService.java
@@ -5,4 +5,6 @@ public interface IEmailService {
     void sendNewMovieNotificationEmail(String to, String movieTitle, String description, String releaseDate);
     void sendMovieUpdatedNotificationEmail(String to, String movieTitle, String description);
     void sendMovieDeletedNotificationEmail(String to, String movieTitle);
+    void sendPaymentSuccessEmail(String to, String bookingId, String paymentMethod, String amount);
+    void sendPaymentFailedEmail(String to, String bookingId, String paymentMethod, String amount, String reason);
 }
